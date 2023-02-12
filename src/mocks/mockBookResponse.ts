03871c72c5a6bd83ca5ff5c26972976885897262
katebry <katebryan31@gmail.com>
2023-02-12 08:33:08 +0000

    Updated mock data

--- src/mocks/mockBookResponse.ts
+++ src/mocks/mockBookResponse.ts
@@ -1,21 +1,29 @@
-import { ApiBookResponse} from "../constants/types";
+import {ApiBookResponse} from "../constants/types";
 
 export const mockBookResponse: ApiBookResponse = {
-  "data": [
-    {
-      "title": "Macbeth",
-      "author": "William Shakespeare",
-      "isbn": "123456789",
-      "quantity": "2",
-      "price": "9.99"
-    },
-    {
-      "title": "Richard II",
-      "author": "William Shakespeare",
-      "isbn": "123456780",
-      "quantity": "9",
-      "price": "11.99"
-    }
-  ],
-  "status": 200
+    "data": [
+        {
+            "book": {
+                "title": "Macbeth",
+                "author": "William Shakespeare",
+                "isbn": "123456789",
+            },
+            "stock": {
+                "quantity": "2",
+                "price": "9.99"
+            }
+        },
+        {
+            "book": {
+                "title": "Richard II",
+                "author": "William Shakespeare",
+                "isbn": "123456780",
+            },
+            "stock": {
+                "quantity": "9",
+                "price": "11.99"
+            }
+        }
+    ],
+    "status": 200
 };
